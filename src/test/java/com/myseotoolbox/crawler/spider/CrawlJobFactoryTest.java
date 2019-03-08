6b106b7c52d023d474b68d4d998f5726460f5eb0
Salvatore Novelli <salvatore.novelli@gmail.com>
2019-03-08 21:57:46 +0000

    Adding testing on filters

--- src/test/java/com/myseotoolbox/crawler/spider/CrawlJobFactoryTest.java
+++ src/test/java/com/myseotoolbox/crawler/spider/CrawlJobFactoryTest.java
@@ -3,6 +3,7 @@ package com.myseotoolbox.crawler.spider;
 import com.myseotoolbox.crawler.PageCrawlPersistence;
 import com.myseotoolbox.crawler.httpclient.SnapshotException;
 import com.myseotoolbox.crawler.httpclient.WebPageReader;
+import com.myseotoolbox.crawler.model.PageSnapshot;
 import com.myseotoolbox.crawler.monitoreduri.MonitoredUriUpdater;
 import com.myseotoolbox.crawler.testutils.CurrentThreadTestExecutorService;
 import com.myseotoolbox.crawler.testutils.PageSnapshotTestBuilder;
@@ -11,20 +12,23 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.net.URI;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CrawlJobFactoryTest {
 
     public static final int SINGLE_THREAD = 1;
+    public static final URI TEST_ORIGIN = URI.create("http://host/base-path/");
+    public static final URI TEST_FILTERED_LINK = TEST_ORIGIN.resolve("path");
 
     @Mock private UriFilter testFilter;
     @Mock private WebPageReader reader;
@@ -32,17 +36,19 @@ public class CrawlJobFactoryTest {
     @Mock private MonitoredUriUpdater monitoredUriUpdater;
     @Mock private PageCrawlPersistence crawlPersistence;
 
+    private ExecutorBuilder executorBuilder = new CurrentThreadExecutorBuilder();
 
     CrawlJobFactory sut;
-    private ExecutorBuilder executorBuilder = new CurrentThreadExecutorBuilder();
-    public static final URI TEST_ORIGIN = URI.create("http://host/base-path/");
 
 
     @Before
     public void setUp() throws Exception {
 
         when(filtersBuilder.buildForOrigin(Mockito.any())).thenReturn(testFilter);
-        when(reader.snapshotPage(any())).thenAnswer(invocation -> PageSnapshotTestBuilder.aPageSnapshotWithStandardValuesForUri(invocation.getArgument(0).toString()));
+        when(reader.snapshotPage(any())).thenAnswer(this::buildSnapshotForUri);
+
+
+        when(testFilter.shouldCrawl(TEST_ORIGIN, TEST_FILTERED_LINK)).thenReturn(false);
 
         sut = new CrawlJobFactory(reader, filtersBuilder, executorBuilder, monitoredUriUpdater, crawlPersistence);
     }
@@ -68,6 +74,17 @@ public class CrawlJobFactoryTest {
         verify(crawlPersistence).persistPageCrawl(argThat(snapshot -> snapshot.getUri().equals(TEST_ORIGIN.toString())));
     }
 
+    @Test
+    public void shouldFilterAsSpecified() throws SnapshotException {
+        CrawlJob job = sut.build(TEST_ORIGIN, SINGLE_THREAD);
+        job.start();
+
+        verify(filtersBuilder).buildForOrigin(TEST_ORIGIN);
+
+        verify(testFilter).shouldCrawl(TEST_ORIGIN, TEST_FILTERED_LINK);
+        verify(reader).snapshotPage(TEST_ORIGIN);
+        verifyNoMoreInteractions(reader);
+    }
 
     //Execute in the test thread instead of spawning a new one
     private class CurrentThreadExecutorBuilder extends ExecutorBuilder {
@@ -75,5 +92,12 @@ public class CrawlJobFactoryTest {
         public ExecutorService buildExecutor(int concurrentConnections) {
             return new CurrentThreadTestExecutorService();
         }
+    }
+
+    private PageSnapshot buildSnapshotForUri(InvocationOnMock invocation) {
+        String uri = invocation.getArgument(0).toString();
+        PageSnapshot snapshot = PageSnapshotTestBuilder.aPageSnapshotWithStandardValuesForUri(uri);
+        snapshot.setLinks(List.of(TEST_FILTERED_LINK.toString()));
+        return snapshot;
     }
 }
