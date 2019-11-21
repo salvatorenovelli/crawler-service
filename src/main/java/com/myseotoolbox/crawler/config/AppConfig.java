e231995eb02e0d35973bbbbfd61872a4d5b22f93
Salvatore Novelli <salvatore.novelli@gmail.com>
2019-11-21 18:22:56 +0000

    Releasing 1.10.1 - Extracted out gcpcommons stuff

--- src/main/java/com/myseotoolbox/crawler/config/AppConfig.java
+++ src/main/java/com/myseotoolbox/crawler/config/AppConfig.java
@@ -1,9 +1,6 @@
 package com.myseotoolbox.crawler.config;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.cloud.gcp.pubsub.support.converter.JacksonPubSubMessageConverter;
-import org.springframework.cloud.gcp.pubsub.support.converter.PubSubMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -14,10 +11,5 @@ public class AppConfig {
     @Bean
     RestTemplate restTemplate() {
         return new RestTemplate();
-    }
-
-    @Bean
-    public PubSubMessageConverter pubSubMessageConverter() {
-        return new JacksonPubSubMessageConverter(new ObjectMapper());
     }
 }
