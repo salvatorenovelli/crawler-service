edee2951c72c42fdd5d8a0f8d5703b8431e0b16b
Salvatore Novelli <salvatore.novelli@gmail.com>
2023-10-25 20:30:54 +0100

    adding listener for OutboundLinkPersistence

---
+++ src/main/java/com/myseotoolbox/crawler/spider/event/OutboundLinkPersistenceEventListener.java
@@ -0,0 +1,26 @@
+package com.myseotoolbox.crawler.spider.event;
+
+import com.myseotoolbox.crawler.model.PageSnapshot;
+import com.myseotoolbox.crawler.spider.PubSubEventDispatch;
+import com.myseotoolbox.crawler.websitecrawl.WebsiteCrawl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import static com.myseotoolbox.crawler.utils.FunctionalExceptionUtils.runOrLogWarning;
+
+
+@RequiredArgsConstructor
+@Slf4j
+@Component
+public class OutboundLinkPersistenceEventListener {
+    private final PubSubEventDispatch pubSubEventDispatch;
+
+    @EventListener
+    public void onPageCrawledEvent(PageCrawledEvent event) {
+        PageSnapshot snapshot = event.getCrawlResult().getPageSnapshot();
+        WebsiteCrawl websiteCrawl = event.getWebsiteCrawl();
+        runOrLogWarning(() -> pubSubEventDispatch.pageCrawlCompletedEvent(websiteCrawl.getId().toHexString(), snapshot), "Error while persisting crawl for uri: " + snapshot.getUri());
+    }
+}
---
+++ src/test/java/com/myseotoolbox/crawler/spider/event/OutboundLinkPersistenceEventListenerTest.java
@@ -0,0 +1,26 @@
+package com.myseotoolbox.crawler.spider.event;
+
+import com.myseotoolbox.crawler.spider.PubSubEventDispatch;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static com.myseotoolbox.crawler.spider.event.PageCrawledEventTestBuilder.aTestPageCrawledEvent;
+import static org.mockito.Mockito.verify;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class OutboundLinkPersistenceEventListenerTest {
+
+    @InjectMocks OutboundLinkPersistenceEventListener sut;
+    @Mock private PubSubEventDispatch pubSubEventDispatch;
+
+    @Test
+    public void onPageCrawledEventShouldPersistOutboundLink() {
+        PageCrawledEvent event = aTestPageCrawledEvent().withStandardValuesForPath("/dst").build();
+        sut.onPageCrawledEvent(event);
+        verify(pubSubEventDispatch).pageCrawlCompletedEvent(event.getWebsiteCrawl().getId().toHexString(), event.getCrawlResult().getPageSnapshot());
+    }
+}
