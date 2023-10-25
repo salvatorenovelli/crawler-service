47d68fa54cb36135692b4ebfbbcce6123345e375
Salvatore Novelli <salvatore.novelli@gmail.com>
2023-10-25 20:23:12 +0100

    decoupling listeners

---
+++ src/main/java/com/myseotoolbox/crawler/spider/event/MonitoredUriUpdatedListener.java
@@ -0,0 +1,25 @@
+package com.myseotoolbox.crawler.spider.event;
+
+import com.myseotoolbox.crawler.model.PageSnapshot;
+import com.myseotoolbox.crawler.monitoreduri.MonitoredUriUpdater;
+import com.myseotoolbox.crawler.websitecrawl.WebsiteCrawl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import static com.myseotoolbox.crawler.utils.FunctionalExceptionUtils.runOrLogWarning;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MonitoredUriUpdatedListener {
+    private final MonitoredUriUpdater monitoredUriUpdater;
+
+    @EventListener
+    public void onPageCrawledEvent(PageCrawledEvent event) {
+        PageSnapshot snapshot = event.getCrawlResult().getPageSnapshot();
+        WebsiteCrawl websiteCrawl = event.getWebsiteCrawl();
+        runOrLogWarning(() -> monitoredUriUpdater.updateCurrentValue(websiteCrawl, snapshot), "Error while updating monitored uris for uri: " + snapshot.getUri());
+    }
+}
---
+++ src/test/java/com/myseotoolbox/crawler/spider/event/MonitoredUriUpdatedListenerTest.java
@@ -0,0 +1,27 @@
+package com.myseotoolbox.crawler.spider.event;
+
+import com.myseotoolbox.crawler.monitoreduri.MonitoredUriUpdater;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static com.myseotoolbox.crawler.spider.event.PageCrawledEventTestBuilder.aTestPageCrawledEvent;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MonitoredUriUpdatedListenerTest {
+
+    @InjectMocks MonitoredUriUpdatedListener sut;
+    @Mock private MonitoredUriUpdater monitoredUriUpdater;
+
+
+    @Test
+    public void onPageCrawledEventShouldUpdateMonitoredUri() {
+        PageCrawledEvent event = aTestPageCrawledEvent().withStandardValuesForPath("/dst").build();
+        sut.onPageCrawledEvent(event);
+        verify(monitoredUriUpdater).updateCurrentValue(event.getWebsiteCrawl(), event.getCrawlResult().getPageSnapshot());
+    }
+
+}
