d86cce30a6659c8485fec299e2e44d87af5ed2d8
Salvatore Novelli <salvatore.novelli@gmail.com>
2023-10-29 14:32:32 +0000

    updating CrawlEventDispatch.java to only leverage spring event system

--- src/main/java/com/myseotoolbox/crawler/spider/event/CrawlEventDispatch.java
+++ src/main/java/com/myseotoolbox/crawler/spider/event/CrawlEventDispatch.java
@@ -1,39 +1,32 @@
 package com.myseotoolbox.crawler.spider.event;
 
 import com.myseotoolbox.crawler.model.CrawlResult;
-import com.myseotoolbox.crawler.model.PageSnapshot;
-import com.myseotoolbox.crawler.monitoreduri.MonitoredUriUpdater;
-import com.myseotoolbox.crawler.pagelinks.OutboundLinksPersistenceListener;
-import com.myseotoolbox.crawler.spider.PubSubEventDispatch;
-import com.myseotoolbox.crawler.websitecrawl.CrawlStartedEvent;
 import com.myseotoolbox.crawler.websitecrawl.WebsiteCrawl;
-import com.myseotoolbox.crawler.websitecrawl.WebsiteCrawlRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import static com.myseotoolbox.crawler.utils.FunctionalExceptionUtils.runOrLogWarning;
+import org.springframework.context.ApplicationEventPublisher;
 
 @Slf4j
 @RequiredArgsConstructor
 public class CrawlEventDispatch {
 
     private final WebsiteCrawl websiteCrawl;
-    private final MonitoredUriUpdater monitoredUriUpdater;
-    private final OutboundLinksPersistenceListener outLinkPersistenceListener;
-    private final WebsiteCrawlRepository websiteCrawlRepository;
-    private final PubSubEventDispatch pubSubEventDispatch;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
 
-    public void pageCrawled(CrawlResult crawlResult) {
-        PageSnapshot snapshot = crawlResult.getPageSnapshot();
-        log.debug("Persisting page crawled: {}", snapshot.getUri());
-        runOrLogWarning(() -> monitoredUriUpdater.updateCurrentValue(websiteCrawl, snapshot), "Error while updating monitored uris for uri: " + snapshot.getUri());
-        runOrLogWarning(() -> pubSubEventDispatch.pageCrawlCompletedEvent(websiteCrawl.getId().toHexString(), snapshot), "Error while persisting crawl for uri: " + snapshot.getUri());
-        runOrLogWarning(() -> outLinkPersistenceListener.accept(crawlResult), "Error while persisting outbound links for uri: " + crawlResult.getUri());
+    public void onPageCrawled(CrawlResult crawlResult) {
+        log.debug("Persisting page crawled: {}", crawlResult.getUri());
+        applicationEventPublisher.publishEvent(new PageCrawledEvent(websiteCrawl, crawlResult));
     }
 
-    public void crawlStarted(CrawlStartedEvent event) {
-        runOrLogWarning(() -> websiteCrawlRepository.save(WebsiteCrawl.fromCrawlStartedEvent(websiteCrawl.getId(), event)), "Error while persisting CrawlStartedEvent: " + event);
+    public void onCrawlStarted() {
+        log.debug("Crawl started event for: {}", websiteCrawl);
+        applicationEventPublisher.publishEvent(new CrawlStartedEvent(websiteCrawl));
+    }
+
+    public void onCrawlCompleted() {
+        log.debug("Crawl completed event for: {}", websiteCrawl);
+        applicationEventPublisher.publishEvent(new CrawlCompletedEvent(websiteCrawl));
     }
 
     public void crawlEnded() {
