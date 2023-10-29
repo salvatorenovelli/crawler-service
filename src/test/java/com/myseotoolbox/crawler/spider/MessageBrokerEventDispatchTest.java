cf54bfd51f05efd0bb427ce04e39c832cfb72ff0
Salvatore Novelli <salvatore.novelli@gmail.com>
2023-10-29 13:06:06 +0000

    adapting MessageBrokerEventDispatch and test

--- src/test/java/com/myseotoolbox/crawler/spider/MessageBrokerEventDispatchTest.java
+++ src/test/java/com/myseotoolbox/crawler/spider/MessageBrokerEventDispatchTest.java
@@ -1,19 +1,19 @@
 package com.myseotoolbox.crawler.spider;
 
-import com.myseotoolbox.crawler.model.CrawlCompletedEvent;
+import com.myseotoolbox.crawler.spider.event.CrawlCompletedEvent;
 import com.myseotoolbox.crawler.model.PageCrawlCompletedEvent;
 import com.myseotoolbox.crawler.model.PageSnapshot;
 import com.myseotoolbox.crawler.spider.configuration.PubSubProperties;
+import com.myseotoolbox.crawler.spider.event.MessageBrokerEventDispatch;
 import com.myseotoolbox.crawler.websitecrawl.WebsiteCrawl;
 import com.myseotoolbox.crawler.websitecrawl.WebsiteCrawlFactory;
-import org.bson.types.ObjectId;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.cloud.gcp.pubsub.core.publisher.PubSubPublisherTemplate;
 
-import java.time.LocalDateTime;
 import java.util.Collections;
 
 import static org.mockito.ArgumentMatchers.eq;
@@ -21,17 +21,23 @@ import static org.mockito.Mockito.verify;
 
 
 @RunWith(MockitoJUnitRunner.class)
-public class PubSubEventDispatchTest {
+public class MessageBrokerEventDispatchTest {
 
 
     public static final String PAGE_CRAWL_COMPLETED_TOPIC = "pageCrawlCompletedTopic";
     public static final String WEBSITE_CRAWL_COMPLETED_TOPIC = "websiteCrawlCompletedTopic";
     @Mock private PubSubPublisherTemplate template;
     private PubSubProperties config = new PubSubProperties(WEBSITE_CRAWL_COMPLETED_TOPIC, PAGE_CRAWL_COMPLETED_TOPIC, 10);
+    MessageBrokerEventDispatch sut;
+
+    @Before
+    public void setUp() throws Exception {
+        sut = new MessageBrokerEventDispatch(template, config);
+    }
 
     @Test
     public void shouldPublishWebsiteCompletedOnTheCorrectQueue() {
-        PubSubEventDispatch sut = new PubSubEventDispatch(template, config);
+
         PageSnapshot val = new PageSnapshot();
         val.setUri("http://host/someuri");
         sut.pageCrawlCompletedEvent("123", val);
@@ -40,9 +46,8 @@ public class PubSubEventDispatchTest {
 
     @Test
     public void shouldPublishPageCrawlCompletedOnTheCorrectQueue() {
-        PubSubEventDispatch sut = new PubSubEventDispatch(template, config);
         WebsiteCrawl websiteCrawl = WebsiteCrawlFactory.newWebsiteCrawlFor("host.host", Collections.emptyList());
-        sut.websiteCrawlCompletedEvent(websiteCrawl);
+        sut.onCrawlCompletedEvent(new CrawlCompletedEvent(websiteCrawl));
         verify(template).publish(eq(WEBSITE_CRAWL_COMPLETED_TOPIC), eq(new CrawlCompletedEvent(websiteCrawl)));
     }
 
