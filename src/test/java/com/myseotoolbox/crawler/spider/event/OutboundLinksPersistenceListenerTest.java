36327d5cdc53a2f86958aae21398a40cf23e72a6
Salvatore Novelli <salvatore.novelli@gmail.com>
2023-11-04 15:22:23 +0000

    accidentally refactored into another class....

---
+++ src/test/java/com/myseotoolbox/crawler/spider/event/OutboundLinksPersistenceListenerTest.java
@@ -0,0 +1,429 @@
+package com.myseotoolbox.crawler.spider.event;
+
+import com.myseotoolbox.crawler.model.CrawlResult;
+import com.myseotoolbox.crawler.model.PageSnapshot;
+import com.myseotoolbox.crawler.model.RedirectChainElement;
+import com.myseotoolbox.crawler.pagelinks.LinkType;
+import com.myseotoolbox.crawler.pagelinks.OutboundLinkRepository;
+import com.myseotoolbox.crawler.pagelinks.OutboundLinks;
+import com.myseotoolbox.crawler.pagelinks.PageLink;
+import com.myseotoolbox.crawler.websitecrawl.WebsiteCrawl;
+import com.myseotoolbox.crawler.websitecrawl.WebsiteCrawlFactory;
+import org.bson.types.ObjectId;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.net.URI;
+import java.util.function.Consumer;
+
+import static com.myseotoolbox.crawler.testutils.PageSnapshotTestBuilder.aTestPageSnapshotForUri;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonMap;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OutboundLinksPersistenceListenerTest {
+
+    private static final String TEST_ORIGIN = "http://domain";
+    private static final URI CRAWL_ORIGIN = URI.create(TEST_ORIGIN);
+    @Mock OutboundLinkRepository repository;
+    public static final ObjectId TEST_CRAWL_ID = new ObjectId();
+    private OutboundLinksPersistenceListener sut;
+
+    @Before
+    public void setUp() {
+        sut = new OutboundLinksPersistenceListener(repository);
+    }
+
+    @Test
+    public void shouldSaveDiscoveredLinks() {
+        CrawlResult crawlResult = givenCrawlResultForPageWithLinks("/relativeLink", "http://absoluteLink/hello");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getCrawlId(), equalTo(TEST_CRAWL_ID));
+            assertThat(outboundLinks.getUrl(), equalTo(TEST_ORIGIN));
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("/relativeLink", "http://absoluteLink/hello"));
+        });
+    }
+
+
+    @Test
+    public void shouldNotPersistDuplicateLinks() {
+        CrawlResult crawlResult = givenCrawlResultForPageWithLinks("/relativeLink", "/relativeLink", "http://absoluteLink/hello", "http://absoluteLink/hello");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getCrawlId(), equalTo(TEST_CRAWL_ID));
+            assertThat(outboundLinks.getUrl(), equalTo("http://domain"));
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("/relativeLink", "http://absoluteLink/hello"));
+        });
+    }
+
+    @Test
+    public void shouldBeFineWithNullLinks() {
+        CrawlResult crawlResult = CrawlResult.forSnapshot(aTestPageSnapshotForUri("http://testuri").withNullLinks().build());
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getCrawlId(), equalTo(TEST_CRAWL_ID));
+            assertThat(outboundLinks.getUrl(), equalTo("http://testuri"));
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), hasSize(0));
+        });
+    }
+
+    @Test
+    public void shouldSkipFailedCrawls() {
+        CrawlResult crawlResult = CrawlResult.forSnapshot(
+                aTestPageSnapshotForUri("http://testuri")
+                        .withEmptyRedirectChain()
+                        .build());
+
+        processCrawlResult(crawlResult);
+
+        verify(repository, Mockito.never()).save(ArgumentMatchers.any(OutboundLinks.class));
+    }
+
+    @Test
+    public void shouldNotPersistFragments() {
+        CrawlResult crawlResult = givenCrawlResultForPageWithLinks("#this-is-a-fragment", "http://absoluteLink/hello#fragment");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("http://absoluteLink/hello"));
+        });
+    }
+
+    @Test
+    public void shouldPersistParameters() {
+        CrawlResult crawlResult = givenCrawlResultForPageWithLinks("http://domain/index?param=true", "http://domain/index?param=false", "http://domain/index");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("/index?param=true", "/index?param=false", "/index"));
+        });
+    }
+
+    @Test
+    public void shouldPersistParametersForExternalDomains() {
+        CrawlResult crawlResult = givenCrawlResultForPageWithLinks("http://externaldomain/index.php?param=true", "http://externaldomain/index.php?param=false");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("http://externaldomain/index.php?param=true", "http://externaldomain/index.php?param=false"));
+        });
+    }
+
+
+    @Test
+    public void shouldNotMergeLeadingSlashWithNon() {
+        CrawlResult crawlResult = givenCrawlResultForPageWithLinks("http://domain/index", "http://domain/index/");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("/index", "/index/"));
+        });
+    }
+
+    @Test
+    public void shouldPersistCanonicals() {
+        CrawlResult crawlResult = givenCrawlResultForPageWithCanonicals("http://domain/it/canonical1", "http://domain/it/canonical2");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.CANONICAL), containsInAnyOrder("/it/canonical1", "/it/canonical2"));
+        });
+    }
+
+    @Test
+    public void shouldNotPersistNoFollow() {
+
+        PageLink notFollowable = new PageLink("/notFollowable", singletonMap("rel", "nofollow"));
+        PageLink followable = new PageLink("/followable", singletonMap("rel", "nex"));
+
+        CrawlResult crawlResult = givenCrawlResultForPageWithNoFollowLinks(notFollowable, followable);
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), hasSize(1));
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF).get(0), is("/followable"));
+        });
+    }
+
+    @Test
+    public void shouldDedupPagesWithFragments() {
+        CrawlResult crawlResult = givenCrawlResultForPageWithLinks("http://host/hello", "http://host/hello#fragment");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("http://host/hello"));
+        });
+    }
+
+    @Test
+    public void canHandleEmptyFragments() {
+        CrawlResult crawlResult = givenCrawlResultForPageWithLinks("#", "http://host/page#");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("http://host/page"));
+        });
+    }
+
+    @Test
+    public void shouldAlwaysPersistRelativeIfIsSameDomain() {
+        CrawlResult crawlResult = givenCrawlResultForUrlWithPageWithLinks("http://domain/some/path",
+                "/link1",
+                "http://domain/link2",
+                "http://domain/some/path/link3",
+                "http://anotherdomain/link1");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("/link1", "/link2", "/some/path/link3", "http://anotherdomain/link1"));
+        });
+    }
+
+    @Test
+    public void shouldBeAbleToRelativizeUrlsWithSpaces() {
+        CrawlResult crawlResult = givenCrawlResultForUrlWithPageWithLinks("http://domain/some/path",
+                "http://domain/link1 with spaces/salve");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("/link1%20with%20spaces/salve"));
+        });
+    }
+
+
+    @Test
+    public void shouldBeAbleToRelativizeUrlsUnicodeCharacters() {
+        CrawlResult crawlResult = givenCrawlResultForUrlWithPageWithLinks("http://domain/some/path",
+                "http://domain/linkWithUnicode\u200B  \u200B");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("/linkWithUnicode%E2%80%8B%20%20%E2%80%8B"));
+        });
+    }
+
+    @Test
+    public void shouldRelativizeBasedOnOrigin() {
+        CrawlResult crawlResult = givenCrawlResultForUrlWithPageWithLinks("https://domain/some/path", "https://domain/link");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("https://domain/link"));
+        });
+    }
+
+    @Test
+    public void shouldSaveAbsoluteLinksIfNonSameDomainAsOrigin() {
+        CrawlResult crawlResult = givenCrawlResultForUrlWithPageWithLinks("https://domain/some/path", "/link");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("https://domain/link"));
+        });
+    }
+
+    @Test
+    public void shouldPersistLinksWithSpacesAtTheEnd() {
+        CrawlResult crawlResult = givenCrawlResultForUrlWithPageWithLinks("http://domain", "http://domain/path ");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("/path"));
+        });
+    }
+
+    @Test
+    public void shouldRelativizeRelativeToCurrentUrls() {
+        CrawlResult crawlResult = givenCrawlResultForUrlWithPageWithLinks("http://domain/some/path/",
+                "salve");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("/some/path/salve"));
+        });
+    }
+
+    @Test
+    public void differentSchemaIsConsideredDifferentHost() {
+        CrawlResult crawlResult = givenCrawlResultForUrlWithPageWithLinks("http://domain/some/path",
+                "/link1",
+                "https://domain/link2");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("/link1", "https://domain/link2"));
+        });
+    }
+
+    @Test
+    public void linksWithNoSlashAtTheBeginningShouldBeResolvedProperly() {
+        CrawlResult crawlResult = givenCrawlResultForUrlWithPageWithLinks("http://domain/subpath/page", "link1");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("/subpath/link1"));
+        });
+    }
+
+    @Test
+    public void invalidUrlShouldBePersistedAsTheyAre() {
+        CrawlResult crawlResult = givenCrawlResultForUrlWithPageWithLinks("http://domain/some/path",
+                "http:-/link1__(this is invalid)__",
+                "https://domain/link2");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("http:-/link1__(this%20is%20invalid)__", "https://domain/link2"));
+        });
+    }
+
+    @Test
+    public void shouldPersistDomain() {
+        CrawlResult crawlResult = givenCrawlResultForUrlWithPageWithLinks("http://something.domain/some/path", "/link1");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getDomain(), is("something.domain"));
+        });
+    }
+
+    @Test
+    public void shouldNotPersistJavascriptLinks() {
+        CrawlResult crawlResult = givenCrawlResultForUrlWithPageWithLinks("http://domain",
+                "/link1",
+                "javascript:void(0)");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("/link1"));
+        });
+    }
+
+    @Test
+    public void linksShouldBeSorted() {
+        CrawlResult crawlResult = givenCrawlResultForUrlWithPageWithLinks("http://domain",
+                "/c", "/d", "/c", "/b", "http://domain/a", "http://anotherdomain/b", "http://anotherdomain/a");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), Matchers.contains("/a", "/b", "/c", "/d", "http://anotherdomain/a", "http://anotherdomain/b"));
+        });
+    }
+
+    @Test
+    public void shouldMaintainTrailingSlashIfPresent() {
+        CrawlResult crawlResult = givenCrawlResultForUrlWithPageWithLinks("http://domain",
+                "/link1/", "/link1");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("/link1", "/link1/"));
+        });
+    }
+
+    @Test
+    public void shouldSaveCrawlDate() {
+        CrawlResult crawlResult = givenCrawlResultForUrlWithPageWithLinks("http://domain",
+                "/link1/", "/link1");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertNotNull(outboundLinks.getCrawledAt());
+        });
+    }
+
+    @Test
+    public void shouldResolveLinksBasedOnDestinationUrl() {
+        CrawlResult crawlResult = givenCrawlResultWithRedirect("http://domain", "https://domain", "/link1", "/link2");
+
+        processCrawlResult(crawlResult);
+
+        verifySavedLinks(outboundLinks -> {
+            assertThat(outboundLinks.getLinksByType().get(LinkType.AHREF), containsInAnyOrder("https://domain/link1", "https://domain/link2"));
+        });
+    }
+
+    private void verifySavedLinks(Consumer<OutboundLinks> linksVerify) {
+        verify(repository).save(ArgumentMatchers.argThat(argument -> {
+            linksVerify.accept(argument);
+            return true;
+        }));
+    }
+
+    private CrawlResult givenCrawlResultForPageWithCanonicals(String... canonicals) {
+        return CrawlResult.forSnapshot(aTestPageSnapshotForUri(TEST_ORIGIN)
+                .withRedirectChainElements(new RedirectChainElement(TEST_ORIGIN, 200, TEST_ORIGIN))
+                .withCanonicals(canonicals).build());
+    }
+
+    private CrawlResult givenCrawlResultForPageWithNoFollowLinks(PageLink... links) {
+        return CrawlResult.forSnapshot(
+                aTestPageSnapshotForUri(TEST_ORIGIN)
+                        .withRedirectChainElements(new RedirectChainElement(TEST_ORIGIN, 200, TEST_ORIGIN))
+                        .withLinks(links)
+                        .build()
+        );
+    }
+
+    private CrawlResult givenCrawlResultForPageWithLinks(String... links) {
+        return givenCrawlResultForUrlWithPageWithLinks(TEST_ORIGIN, links);
+    }
+
+    private CrawlResult givenCrawlResultWithRedirect(String url, String destinationUrl, String... links) {
+        PageSnapshot build = aTestPageSnapshotForUri(url)
+                .withRedirectChainElements(new RedirectChainElement(url, 301, destinationUrl), new RedirectChainElement(destinationUrl, 200, destinationUrl))
+                .withLinks(links).build();
+        return CrawlResult.forSnapshot(build);
+    }
+
+    private CrawlResult givenCrawlResultForUrlWithPageWithLinks(String url, String... links) {
+        return CrawlResult.forSnapshot(aTestPageSnapshotForUri(url)
+                .withRedirectChainElements(new RedirectChainElement(url, 200, url))
+                .withLinks(links).build());
+    }
+
+    private void processCrawlResult(CrawlResult crawlResult) {
+        WebsiteCrawl websiteCrawl = WebsiteCrawlFactory.newWebsiteCrawlFor(TEST_CRAWL_ID, CRAWL_ORIGIN.toASCIIString(), emptyList());
+        sut.onPageCrawled(new PageCrawledEvent(websiteCrawl, crawlResult));
+    }
+}
