a9b27feb001f6427766010c00752b3a5b2b70f5b
Salvatore Novelli <salvatore.novelli@gmail.com>
2024-05-27 13:28:31 +0100

    using more efficient notation

--- src/main/java/com/myseotoolbox/CrawlerApplication.java
+++ src/main/java/com/myseotoolbox/CrawlerApplication.java
@@ -13,7 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Slf4j
 public class CrawlerApplication {
     public static void main(String[] args) {
-        Thread.setDefaultUncaughtExceptionHandler((t, e) -> log.error("Uncaught exception in thread " + t, e));
+        Thread.setDefaultUncaughtExceptionHandler((t, e) -> log.error("Uncaught exception in thread {}", t, e));
         SpringApplication.run(CrawlerApplication.class, args);
     }
 }
