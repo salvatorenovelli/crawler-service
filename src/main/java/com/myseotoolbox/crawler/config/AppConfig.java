5ffb8da3d8e47afc2623d3f5470a9ee408f44c68
Salvatore Novelli <salvatore.novelli@gmail.com>
2024-07-14 22:04:14 +0100

    tolerant reader is by default and creating obect mapper screws the sensible defaults spring put in place

--- src/main/java/com/myseotoolbox/crawler/config/AppConfig.java
+++ src/main/java/com/myseotoolbox/crawler/config/AppConfig.java
@@ -1,8 +1,6 @@
 package com.myseotoolbox.crawler.config;
 
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -15,11 +13,5 @@ public class AppConfig {
         return new RestTemplate();
     }
 
-    @Bean
-    public ObjectMapper getObjectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        return objectMapper;
-    }
 
 }
--- src/test/java/com/myseotoolbox/crawler/config/ObjectMapperTolerantReaderTest.java
+++ src/test/java/com/myseotoolbox/crawler/config/ObjectMapperTolerantReaderTest.java
@@ -2,15 +2,22 @@ package com.myseotoolbox.crawler.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class ObjectMapperTolerantReaderTest {
-    @Test
-    public void shouldNotFailIfNewFieldsAreAdded() throws Exception {
-        String jsonWithUnknownField = "{ \"knownField\": \"value\", \"unknownField\": \"extraValue\" }";
 
-        ObjectMapper objectMapper = new AppConfig().getObjectMapper();
+    @Autowired ObjectMapper objectMapper;
+
+    @Test
+    public void shouldImplementTolerantReader() throws Exception {
+        String jsonWithUnknownField = "{ \"knownField\": \"value\", \"unknownField\": \"extraValue\" }";
 
         MyEvent event = objectMapper.readValue(jsonWithUnknownField, MyEvent.class);
 
@@ -25,6 +32,7 @@ public class ObjectMapperTolerantReaderTest {
         public String getKnownField() {
             return knownField;
         }
+
         public void setKnownField(String knownField) {
             this.knownField = knownField;
         }
