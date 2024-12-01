2662c1b0b90f43c160c6a1dbf94d53ab1759dcea
Salvatore Novelli <salvatore.novelli@gmail.com>
2024-12-01 17:20:07 +0000

    will take me less to find it next time...

--- src/test/java/com/myseotoolbox/crawler/spider/WorkspaceCrawlerIntegrationExploratoryTest.java
+++ src/test/java/com/myseotoolbox/crawler/spider/WorkspaceCrawlerIntegrationExploratoryTest.java
@@ -31,7 +31,7 @@ import static org.mockito.Mockito.*;
 
 
 @RunWith(MockitoJUnitRunner.class)
-public class WorkspaceCrawlerIntegrationTest {
+public class WorkspaceCrawlerIntegrationExploratoryTest {
 
     private ClockUtils testClockUtils = new TestClockUtils();
 
