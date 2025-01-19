cd1942917b3a35f01082454fa0cb279325529cc6
Salvatore Novelli <salvatore.novelli@gmail.com>
2025-01-19 17:42:56 +0000

    removing redundant dependency

--- src/main/java/com/myseotoolbox/crawler/config/CrawlersPoolFactory.java
+++ src/main/java/com/myseotoolbox/crawler/config/CrawlersPoolFactory.java
@@ -2,7 +2,6 @@ package com.myseotoolbox.crawler.config;
 
 import com.myseotoolbox.crawler.httpclient.WebPageReader;
 import com.myseotoolbox.crawler.spider.CrawlersPool;
-import com.myseotoolbox.crawler.spider.sitemap.SitemapRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,11 +10,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 @Component
 @RequiredArgsConstructor
 public class CrawlersPoolFactory {
-
-    private final SitemapRepository sitemapRepository;
-
     public CrawlersPool create(WebPageReader webPageReader, ThreadPoolExecutor executor) {
-        return new CrawlersPool(webPageReader, executor, sitemapRepository);
+        return new CrawlersPool(webPageReader, executor);
     }
-
 }
